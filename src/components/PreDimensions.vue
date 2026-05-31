198653c9191ef965b3ffd75b4587271e99419665
Leonardo Silveira <sombriks@gmail.com>
2026-05-31 17:20:46 -0300

    canvas options

---
+++ src/components/PreDimensions.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {defineModel} from "vue"
+
+  const width = defineModel('width')
+  const height = defineModel('height')
+</script>
+<template>
+  <fieldset>
+    <legend>Dimensions</legend>
+    <input type="number" v-model="width"/> X <input type="number" v-model="height"/>
+  </fieldset>
+</template>
+<style scoped>
+fieldset {
+  display: flex;
+  align-items: center;
+  max-width: 12rem;
+}
+fieldset>input {
+  margin-left: 1rem;
+  margin-right: 1rem;
+}
+</style>
