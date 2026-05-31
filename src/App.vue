e0437d0d79d01250552a3ee9e8fed1564e53a27c
Leonardo Silveira <sombriks@gmail.com>
2026-05-31 13:08:10 -0300

    ok o parcel permite umas abominações interessantes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <h1>hello world</h1>
+  <h1>hello world from vue</h1>
+  <button @click="count++">Count is: {{count}}</button>
 </template>
 <style>
-@import 'npm:@owickstrom/the-monospace-web/src/reset.css';
-@import 'npm:@owickstrom/the-monospace-web/src/index.css';
 </style>
 <script setup>
-  console.log('hello world')
+  import {ref} from "vue";
+
+  const count = ref(0)
+  console.log('hello world from vue')
 </script>
