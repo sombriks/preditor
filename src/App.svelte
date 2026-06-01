936924c18e47fc3a81bd0967b94e141395618fbf
Leonardo Silveira <sombriks@gmail.com>
2026-06-01 20:58:27 -0300

    svelte also fails

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  let count = $state(0);
+  console.log('Hello World from svelte')
+</script>
+<h1>Hello World from svelte</h1>
+<button onclick={() => count += 1}>
+    Count is: {count}
+</button>
